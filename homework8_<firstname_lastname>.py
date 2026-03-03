4f1c7fa6f9a2b6915fc17cf5c07d188056918dcc
gina0022 <gina.steinman@outlook.com>
2026-03-03 04:41:43 +0000

    homework 8

---
+++ homework8_<firstname_lastname>.py
@@ -0,0 +1,7 @@
+def make_sandwich(*ingredients):
+    print("This sandwhich has:")
+    for item in ingredients:
+        print (item)
+make_sandwich("turkey", "cheese")
+make_sandwich("italian", "pepperoni", "salami", "ham", "dressing")
+        
